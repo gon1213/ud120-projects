063e7e8307a753ffc132a72e30f73d82e067fa40
gon1213 <gon901213@gmail.com>
2016-11-03 18:54:10 -0700

    Finish navie bayes training using GaussianNB

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -26,8 +26,20 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
+
+clf = GaussianNB()
+# set time = 0
+t0 = time()
 
 
+clf.fit(features_train,labels_train)
+pred=clf.predict(features_test)
+# print time used 
+print "training time:", round(time()-t0, 3), "s"
+#print accuracy score
+print "accuracy:",accuracy_score(pred,labels_test)
 #########################################################
 
 
