b663970c1a8254f061e852cd1044cfd203a6561f
gon1213 <gon901213@gmail.com>
2016-12-04 02:56:55 -0800

    finish outlier mini project

--- outliers/outlier_removal_regression.py
+++ outliers/outlier_removal_regression.py
@@ -20,15 +20,21 @@ net_worths = pickle.load( open("practice_outliers_net_worths.pkl", "r") )
 ### and n_columns is the number of features
 ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
 net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 ages_train, ages_test, net_worths_train, net_worths_test = train_test_split(ages, net_worths, test_size=0.1, random_state=42)
 
 ### fill in a regression here!  Name the regression object reg so that
 ### the plotting code below works, and you can see what your regression looks like
 
+from sklearn import linear_model
+reg = linear_model.LinearRegression()
+reg.fit(ages_train, net_worths_train)
 
+# print "slope: ", reg.coef_
+# print "y-intercept: ", reg.intercept_
 
-
+# print "training's score: :", reg.score(ages_train,net_worths_train)
+# print "test's score :", reg.score(ages_test,net_worths_test)
 
 
 
@@ -65,6 +71,14 @@ if len(cleaned_data) > 0:
     ages       = numpy.reshape( numpy.array(ages), (len(ages), 1))
     net_worths = numpy.reshape( numpy.array(net_worths), (len(net_worths), 1))
 
+
+    reg = linear_model.LinearRegression()
+    reg.fit(ages, net_worths)
+    print "slope: ", reg.coef_
+    print "y-intercept: ", reg.intercept_
+
+    print "cleaned data's score: :", reg.score(ages, net_worths)
+    print "new test's score :", reg.score(ages_test,net_worths_test)
     ### refit your cleaned data!
     try:
         reg.fit(ages, net_worths)
@@ -82,3 +96,4 @@ if len(cleaned_data) > 0:
 else:
     print "outlierCleaner() is returning an empty list, no refitting to be done"
 
+print "finish line -------------------------------------------------------------"
